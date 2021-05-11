c1d4c204c2dff5616040b807deccfe02bd81c063
Kubilay Humm <lieqb2@gmail.com>
2021-05-11 03:08:53 +0300

    settings before build

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl =
+  process.env.NODE_ENV !== 'production'
+    ? 'https://mysocial123.herokuapp.com'
+    : 'http://localhost:3000';
